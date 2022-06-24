4dcfeb3abb16f0145784bf74f6ab29fdd63c052b
Nicola Milne <nicolalouisemilne@gmail.com>
2022-06-24 09:41:02 -0600

    created react app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "bulma/css/bulma.min.css";
+
+const App = () => {
+  return (
+    <div classname="main">
+      <div className="buttons">
+        <button class="button is-primary">Primary</button>
+        <button class="button is-link">Link</button>
+        <button class="button is-info">Info</button>
+        <button class="button is-success">Success</button>
+        <button class="button is-warning">Warning</button>
+        <button class="button is-danger">Danger</button>
+        <button class="button is-black">Black</button>
+        <button class="button is-white">White</button>
+        <button class="button is-dark">Dark</button>
+        <button class="button is-light">Light</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
